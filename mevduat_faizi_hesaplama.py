a5559c0295b5bc69242e792fee90444963c6eb47
Çağatay ALSANCAK <24699441+cagatayalsancak@users.noreply.github.com>
2021-04-30 22:14:50 +0300

    Update mevduat_faizi_hesaplama.py

--- mevduat_faizi_hesaplama.py
+++ mevduat_faizi_hesaplama.py
@@ -1,26 +1,28 @@
 print("-----------------------------------------------------\nMevduat Faizi Hesaplama Programi\n-----------------------------------------------------")
 
-anapara = int(input("Lutfen Yatirmak Istediginiz Anaparanin miktarini yaziniz : "))
-faizorani = float(input("Lutfen Mevduatinizin Yillik Faiz oranini yaziniz : "))
-faizsuresi = input("Lutfen yatirmak istediginiz Mevduatin turunu yaziniz (Gunluk/Aylik/Yillik) : ")
-
+anaPara = int(input("Lutfen Yatirmak Istediginiz Anaparanin miktarini yaziniz : "))
+faizOrani = float(input("Lutfen Mevduatinizin Yillik Faiz oranini yaziniz : "))
+mevduatTuru = input("Lutfen yatirmak istediginiz Mevduatin turunu yaziniz (Gunluk/Aylik/Yillik) : ")
+def faizHesapla(anaPara, faizOrani, mevduatTuru, mevduatSuresi, mevduatTS):
+	brutFaiz = (anaPara * faizOrani) / mevduatTS * mevduatSuresi
+	stopaj = brutFaiz * 0.15
+	netFaiz = brutFaiz - stopaj
+	return brutFaiz, stopaj, netFaiz, mevduatSuresi
+	
 if (faizsuresi == "Gunluk" or faizsuresi == "gunluk" ):
-	gunsayisi = int(input("Lutfen Gunluk Mevduatinizin Gun sayisini seciniz (32/35) : "))
-	brutfaiz = (anapara * faizorani) / 36500 * gunsayisi
-	stopaj = brutfaiz * 0.15
-	netfaiz = brutfaiz - stopaj
+	mevduatSuresi = int(input("Lutfen Gunluk Mevduatinizin Gun sayisini seciniz (32/35) : "))
+	mevduatTS = 36500
+	faizHesapla(anaPara, faizOrani, mevduatTuru, mevduatSuresi, mevduatTS)
 	print("{} Günlük brüt faiziniz {} TL, % 15 Vergi Stopaji {} TL, net faiziniz {} TL'dir.".format(gunsayisi,brutfaiz,stopaj,netfaiz))
 elif(faizsuresi == "Aylik" or faizsuresi == "aylik"):
-	aysayisi = int(input("Lutfen Aylik Mevduatinizin Ay sayisini seciniz : "))
-	brutfaiz = (anapara * faizorani) / 1200 * aysayisi
-	stopaj = brutfaiz * 0.15
-	netfaiz = brutfaiz - stopaj
+	mevduatSuresi = int(input("Lutfen Aylik Mevduatinizin Ay sayisini seciniz : "))
+	mevduatTS = 1200
+	faizHesapla(anaPara, faizOrani, mevduatTuru, mevduatSuresi, mevduatTS)
 	print("{} Aylik brüt faiziniz {} TL, % 15 Vergi Stopaji {} TL, net faiziniz {} TL'dir.".format(aysayisi,brutfaiz,stopaj,netfaiz))
 elif(faizsuresi == "Yillik" or faizsuresi == "yillik"):
-	yilsayisi = int(input("Lutfen Yillik Mevduatinizin Yil sayisini seciniz : "))
-	brutfaiz = (anapara * faizorani) / 100 * yilsayisi
-	stopaj = brutfaiz * 0.15
-	netfaiz = brutfaiz - stopaj
+	mevduatSuresi = int(input("Lutfen Yillik Mevduatinizin Yil sayisini seciniz : "))
+	mevduatTS = 100
+	faizHesapla(anaPara, faizOrani, mevduatTuru, mevduatSuresi, mevduatTS)
 	print("{} Yillik brüt faiziniz {} TL, % 15 Vergi Stopaji {} TL, net faiziniz {} TL'dir.".format(yilsayisi,brutfaiz,stopaj,netfaiz))
 else:
 	print("Lutfen Gecerli Mevduat Turu yaziniz!")
